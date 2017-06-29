e5e0a41176f423825ddb8795e35392cf78a8c4fa
tcriscuolo <tcriscuolo@gmail.com>
2017-06-29 11:54:50 -0300

    Up test log

--- logs/mean.sh
+++ logs/mean.sh
@@ -6,7 +6,7 @@ out="${folder}/$what.txt"
 
 rm -f $out
 
-for i in `seq 1 30`
+for i in `seq 1 20`
 do
 	echo "$what ${folder}/execution_${i}.log >> $out"
 	grep $what ${folder}/execution_${i}.log | awk '{print $NF}' >> $out  
